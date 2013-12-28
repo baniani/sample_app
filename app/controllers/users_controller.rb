f29c0d2ba6a423c620ffcd2ce8273feff741bb81
Behdad Baniani <baniani@live.com>
2013-12-27 19:07:52 -0500

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   end
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     #@hi = User.Say_Hello()
   end
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
 	before_save { self.email = email.downcase }
 	before_create :create_remember_token
 	validates :name, presence: true, length: { maximum: 50 }
@@ -10,12 +11,17 @@ class User < ActiveRecord::Base
 	has_secure_password
 
 	def User.new_remember_token
-    	SecureRandom.urlsafe_base64
-  	end
+    SecureRandom.urlsafe_base64
+  end
 
-  	def User.encrypt(token)
-    	Digest::SHA1.hexdigest(token.to_s)
-  	end
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
 
   	private
 
@@ -24,6 +30,6 @@ class User < ActiveRecord::Base
     	end
 	
 	def self.Say_Hello
-       "Salam"
-    end
+    "Salam"
+  end
 end
